0be8cfc75a40aa89cbadc17ddb7bd7f9540f689d
uaburak <design.burakkoc@gmail.com>
2026-02-20 22:42:07 +0300

    Initial iOS project setup with SwiftUI

---
+++ Finvo/Finvo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finvo
+//
+//  Created by Burak KOÇ on 20.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finvo/Finvo/FinvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinvoApp.swift
+//  Finvo
+//
+//  Created by Burak KOÇ on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
